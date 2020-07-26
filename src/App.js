3a9324598c496512c608caf6d48a92c97dd3a5a2
Taimoor khan <khant8k83@gmail.com>
2020-07-26 16:14:58 +0500

    bootcamp continue d

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Room from './Room.js'
 function App() { //props is a javascript object and we can properties of  props object just 
                       // like normal js objects
   return <div className="App">
-      <h1>Welcome to my page</h1><br/>
+      <h1>Continued the bootcamp</h1><br/>
       <Room></Room> 
     </div>
 
